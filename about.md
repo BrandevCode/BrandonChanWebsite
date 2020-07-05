e26aea0c72604d811df33ab0e8167f37b75ff10d
ExtremeGitHubb <37488774+ExtremeGitHubb@users.noreply.github.com>
2020-07-05 11:04:19 -0700

    please work!

---
+++ about.md
@@ -0,0 +1,23 @@
+---
+layout: page
+title: About
+css: ["about.css", "animate.css", "morphext.css", "post.css"]
+js: ["morphext.min.js", "about.js"]
+---
+
+So you've found my website!
+
+If we have never met before, I would like to say nice to meet you and ask who you are, but sadly this page is about me and not about you.
+
+Hi, my name is **Brandon**, and I do not know how to describe myself. This is due to my personality constantly evolving throughout my life. As an example, when I was younger, I was very timid and shy. I was like this because I was afraid of saying something I did not mean and kept my words and feelings to myself. As I grew older, I realized its okay to admit to my mistakes and care a little less about what others think. This gave me more confidence to be more social and true to who I am. Finding who I am is one of the reasons why I created this website and I hope I can continue to evolve and become a better person.
+
+Another reason is to help others by giving them support on topics I am either familiar with or passionate about such as **coding, drawing** and **healthy habits**. I am in no means an expert, but I hope I will be able to spark interest and maybe add to someones life.
+
+Lastly I made this so **employers** can find out more about who I am and what I know. For more info please navigate here: "ADD A LINK HERE BRANDON"
+
+
+Welcome to the site.
+
+<div class="thi-signature" style="color:white">
+    {{ site.user.name }}
+</div>
---
+++ assets/js/main.js
@@ -0,0 +1,85 @@
+//sidenav
+(function($){
+  $(function(){
+    $(".button-collapse").sideNav();
+  });
+})(jQuery);
+
+//parallax
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Programmer", "Designer", "Video Editor", "Gamer", "Guitarist", "Snowboarder"];
+const typingDelay = 135;
+const erasingDelay = 120;
+const newTextDelay = 2250; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (cursorSpan != null)
+  {
+    if (charIndex < textArray[textArrayIndex].length) {
+      if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+      typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingDelay);
+    } 
+    else {
+      cursorSpan.classList.remove("typing");
+      setTimeout(erase, newTextDelay);
+    }
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+//Rotate icon
+
+$(".collapsible-header").click(function () {
+
+  //var rotateClass = this.querySelector('.i las la-caret-down right rotate');
+
+  //$(rotateClass).toggleClass("down");
+
+  $(this).find(".rotate").toggleClass("down");
+
+  console.log("rotate");
+})
+
+//Fade in on scroll
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".fadein");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } 
+  }
+});
